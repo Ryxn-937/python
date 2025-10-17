41db65349ad8526a226f67d7f5282031efe51eff
ryxn <ryanrenjith937@gmail.com>
2025-10-17 15:54:37 +0530

    day4.hw

---
+++ day4.py
@@ -0,0 +1,22 @@
+WD_=["Ramu","Raju","appu"]
+DS_=["john","jacob","jack"]
+UI_=["nick","fury","ryan"]
+all_participants=[WD_,DS_,UI_]
+WD_.append("Tom")
+DS_.insert(2,"Jerry")
+UI_.pop()
+DS_2=DS_.copy()
+DS_.clear()
+print(WD_[:2])
+name_lengths=[]
+for x in DS_2:
+    length=len(x)
+    name_lengths.append(length)
+print("length of the names are:",name_lengths)
+
+if "Asha" in all_participants:
+    print("yes,Asha on the list")
+else:
+    print("no,Asha not on the list")
+first_name=(WD_[0],DS_2[0],UI_[0])
+print(first_name)
